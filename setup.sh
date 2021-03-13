37fa77d42032dfbffa867a0758874897f46a8acd
Pierce <piercedhowell@gmail.com>
2021-03-13 15:36:50 -0800

    Renamed Media to media.

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 source /usr/share/gazebo/setup.sh
 export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:/home/pierce/Projects/mini_auv_sim/models
 export GAZEBO_RESOURCE_PATH=${GAZEBO_RESOURCE_PATH}:/home/pierce/Projects/mini_auv_sim/worlds
-export GAZEBO_RESOURCE_PATH=${GAZEBO_RESOURCE_PATH}:/home/pierce/Projects/mini_auv_sim/Media
+export GAZEBO_RESOURCE_PATH=${GAZEBO_RESOURCE_PATH}:/home/pierce/Projects/mini_auv_sim/media
 export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:/home/pierce/Projects/mini_auv_sim/build/mini_auv_gazebo_plugins
