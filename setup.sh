46585b7ef17083adc8d3dc5f6ac857d280c02f01
Pierce <piercedhowell@gmail.com>
2021-03-20 09:55:35 -0700

    Fixed the setup.sh to source whenever called within the project file path.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+PROJ_DIR=$(pwd)
 source /usr/share/gazebo/setup.sh
-export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:/home/pierce/Projects/mini_auv_sim/models
-export GAZEBO_RESOURCE_PATH=${GAZEBO_RESOURCE_PATH}:/home/pierce/Projects/mini_auv_sim/worlds
-export GAZEBO_RESOURCE_PATH=${GAZEBO_RESOURCE_PATH}:/home/pierce/Projects/mini_auv_sim/media
-export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:/home/pierce/Projects/mini_auv_sim/build/mini_auv_gazebo_plugins
+export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:${PROJ_DIR}/models
+export GAZEBO_RESOURCE_PATH=${GAZEBO_RESOURCE_PATH}:${PROJ_DIR}/worlds
+export GAZEBO_RESOURCE_PATH=${GAZEBO_RESOURCE_PATH}:${PROJ_DIR}/media
+export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:${PROJ_DIR}/build/mini_auv_gazebo_plugins
